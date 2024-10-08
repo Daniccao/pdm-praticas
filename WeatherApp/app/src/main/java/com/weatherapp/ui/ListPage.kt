fc9197b84ee01f3801a70a99a97e329fa0ceb481
danielcardoso <cardosodaniel75@gmail.com>
2024-10-08 20:01:36 -0300

    Pratica 10 - Parte 1 - Adicionando monitoramento

--- WeatherApp/app/src/main/java/com/weatherapp/ui/ListPage.kt
+++ WeatherApp/app/src/main/java/com/weatherapp/ui/ListPage.kt
@@ -15,6 +15,8 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.rounded.FavoriteBorder
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -99,6 +101,12 @@ fun CityItem(
         )
         Spacer(modifier = Modifier.size(12.dp))
         Column(modifier = modifier.weight(1f)) {
+            Icon(
+                imageVector = if (city.isMonitored ==
+                    true) Icons.Outlined.Favorite else Icons.Outlined.FavoriteBorder,
+                contentDescription = "Monitor?",
+                modifier = Modifier.size(16.dp)
+            )
             Text(modifier = Modifier,
                 text = city.name,
                 fontSize = 24.sp)
